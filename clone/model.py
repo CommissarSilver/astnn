8ca967f628433058b144b06502f60b190689c8a7
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-03-27 15:33:50 -0400

    documentation for future

--- clone/model.py
+++ clone/model.py
@@ -6,7 +6,15 @@ import random
 
 
 class BatchTreeEncoder(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, encode_dim, batch_size, use_gpu, pretrained_weight=None):
+    def __init__(
+        self,
+        vocab_size,
+        embedding_dim,
+        encode_dim,
+        batch_size,
+        use_gpu,
+        pretrained_weight=None,
+    ):
         super(BatchTreeEncoder, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
         self.embedding_dim = embedding_dim
@@ -34,36 +42,45 @@ class BatchTreeEncoder(nn.Module):
         size = len(node)
         if not size:
             return None
-        batch_current = self.create_tensor(Variable(torch.zeros(size, self.embedding_dim)))
+        batch_current = self.create_tensor(
+            Variable(torch.zeros(size, self.embedding_dim))
+        )
 
         index, children_index = [], []
         current_node, children = [], []
         for i in range(size):
             # if node[i][0] is not -1:
-                index.append(i)
-                current_node.append(node[i][0])
-                temp = node[i][1:]
-                c_num = len(temp)
-                for j in range(c_num):
-                    if temp[j][0] is not -1:
-                        if len(children_index) <= j:
-                            children_index.append([i])
-                            children.append([temp[j]])
-                        else:
-                            children_index[j].append(i)
-                            children[j].append(temp[j])
-            # else:
-            #     batch_index[i] = -1
+            index.append(i)
+            current_node.append(node[i][0])
+            temp = node[i][1:]
+            c_num = len(temp)
+            for j in range(c_num):
+                if temp[j][0] is not -1:
+                    if len(children_index) <= j:
+                        children_index.append([i])
+                        children.append([temp[j]])
+                    else:
+                        children_index[j].append(i)
+                        children[j].append(temp[j])
+        # else:
+        #     batch_index[i] = -1
 
-        batch_current = self.W_c(batch_current.index_copy(0, Variable(self.th.LongTensor(index)),
-                                                          self.embedding(Variable(self.th.LongTensor(current_node)))))
+        batch_current = self.W_c(
+            batch_current.index_copy(
+                0,
+                Variable(self.th.LongTensor(index)),
+                self.embedding(Variable(self.th.LongTensor(current_node))),
+            )
+        )
 
         for c in range(len(children)):
             zeros = self.create_tensor(Variable(torch.zeros(size, self.encode_dim)))
             batch_children_index = [batch_index[i] for i in children_index[c]]
             tree = self.traverse_mul(children[c], batch_children_index)
             if tree is not None:
-                batch_current += zeros.index_copy(0, Variable(self.th.LongTensor(children_index[c])), tree)
+                batch_current += zeros.index_copy(
+                    0, Variable(self.th.LongTensor(children_index[c])), tree
+                )
         # batch_index = [i for i in batch_index if i is not -1]
         b_in = Variable(self.th.LongTensor(batch_index))
         self.node_list.append(self.batch_node.index_copy(0, b_in, batch_current))
@@ -71,7 +88,9 @@ class BatchTreeEncoder(nn.Module):
 
     def forward(self, x, bs):
         self.batch_size = bs
-        self.batch_node = self.create_tensor(Variable(torch.zeros(self.batch_size, self.encode_dim)))
+        self.batch_node = self.create_tensor(
+            Variable(torch.zeros(self.batch_size, self.encode_dim))
+        )
         self.node_list = []
         self.traverse_mul(x, list(range(self.batch_size)))
         self.node_list = torch.stack(self.node_list)
@@ -79,9 +98,19 @@ class BatchTreeEncoder(nn.Module):
 
 
 class BatchProgramCC(nn.Module):
-    def __init__(self, embedding_dim, hidden_dim, vocab_size, encode_dim, label_size, batch_size, use_gpu=True, pretrained_weight=None):
+    def __init__(
+        self,
+        embedding_dim,
+        hidden_dim,
+        vocab_size,
+        encode_dim,
+        label_size,
+        batch_size,
+        use_gpu=True,
+        pretrained_weight=None,
+    ):
         super(BatchProgramCC, self).__init__()
-        self.stop = [vocab_size-1]
+        self.stop = [vocab_size - 1]
         self.hidden_dim = hidden_dim
         self.num_layers = 1
         self.gpu = use_gpu
@@ -90,12 +119,23 @@ class BatchProgramCC(nn.Module):
         self.embedding_dim = embedding_dim
         self.encode_dim = encode_dim
         self.label_size = label_size
-        self.encoder = BatchTreeEncoder(self.vocab_size, self.embedding_dim, self.encode_dim,
-                                        self.batch_size, self.gpu, pretrained_weight)
+        self.encoder = BatchTreeEncoder(
+            self.vocab_size,
+            self.embedding_dim,
+            self.encode_dim,
+            self.batch_size,
+            self.gpu,
+            pretrained_weight,
+        )
         self.root2label = nn.Linear(self.encode_dim, self.label_size)
         # gru
-        self.bigru = nn.GRU(self.encode_dim, self.hidden_dim, num_layers=self.num_layers, bidirectional=True,
-                            batch_first=True)
+        self.bigru = nn.GRU(
+            self.encode_dim,
+            self.hidden_dim,
+            num_layers=self.num_layers,
+            bidirectional=True,
+            batch_first=True,
+        )
         # linear
         self.hidden2label = nn.Linear(self.hidden_dim * 2, self.label_size)
         # hidden
@@ -105,12 +145,24 @@ class BatchProgramCC(nn.Module):
     def init_hidden(self):
         if self.gpu is True:
             if isinstance(self.bigru, nn.LSTM):
-                h0 = Variable(torch.zeros(self.num_layers * 2, self.batch_size, self.hidden_dim).cuda())
-                c0 = Variable(torch.zeros(self.num_layers * 2, self.batch_size, self.hidden_dim).cuda())
+                h0 = Variable(
+                    torch.zeros(
+                        self.num_layers * 2, self.batch_size, self.hidden_dim
+                    ).cuda()
+                )
+                c0 = Variable(
+                    torch.zeros(
+                        self.num_layers * 2, self.batch_size, self.hidden_dim
+                    ).cuda()
+                )
                 return h0, c0
-            return Variable(torch.zeros(self.num_layers * 2, self.batch_size, self.hidden_dim)).cuda()
+            return Variable(
+                torch.zeros(self.num_layers * 2, self.batch_size, self.hidden_dim)
+            ).cuda()
         else:
-            return Variable(torch.zeros(self.num_layers * 2, self.batch_size, self.hidden_dim))
+            return Variable(
+                torch.zeros(self.num_layers * 2, self.batch_size, self.hidden_dim)
+            )
 
     def get_zeros(self, num):
         zeros = Variable(torch.zeros(num, self.encode_dim))
@@ -119,6 +171,7 @@ class BatchProgramCC(nn.Module):
         return zeros
 
     def encode(self, x):
+        #! it seems the magic of the ccd is happening here
         lens = [len(item) for item in x]
         max_len = max(lens)
 
@@ -132,16 +185,20 @@ class BatchProgramCC(nn.Module):
         for i in range(self.batch_size):
             end += lens[i]
             seq.append(encodes[start:end])
-            if max_len-lens[i]:
-                seq.append(self.get_zeros(max_len-lens[i]))
+            if max_len - lens[i]:
+                seq.append(self.get_zeros(max_len - lens[i]))
             start = end
         encodes = torch.cat(seq)
         encodes = encodes.view(self.batch_size, max_len, -1)
-        encodes = nn.utils.rnn.pack_padded_sequence(encodes, torch.LongTensor(lens), True, False)
+        encodes = nn.utils.rnn.pack_padded_sequence(
+            encodes, torch.LongTensor(lens), True, False
+        )
         # return encodes
 
         gru_out, _ = self.bigru(encodes, self.hidden)
-        gru_out, _ = nn.utils.rnn.pad_packed_sequence(gru_out, batch_first=True, padding_value=-1e9)
+        gru_out, _ = nn.utils.rnn.pad_packed_sequence(
+            gru_out, batch_first=True, padding_value=-1e9
+        )
 
         gru_out = torch.transpose(gru_out, 1, 2)
         # pooling
