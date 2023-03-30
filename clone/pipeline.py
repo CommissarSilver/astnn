e4ba9ab3e0efb78fb52ac8febce5366e19c3195a
Ahura Majdinasab <a.majdinasab@hotmail.com>
2023-03-30 09:00:54 -0400

    fixed the incorrect path situation

--- clone/pipeline.py
+++ clone/pipeline.py
@@ -6,16 +6,16 @@ import click
 from tqdm.auto import tqdm
 
 tqdm.pandas()
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 
 
 class Pipeline:
-    def __init__(self,  ratio, root, language: str):
+    def __init__(self, ratio, root, language: str):
 
         self.language = language.lower()
-        assert self.language in ('c', 'java')
+        assert self.language in ("c", "java")
         self.ratio = ratio
-        self.root = root
+        self.root = os.path.join(os.getcwd(), "clone", root)
         self.sources = None
         self.blocks = None
         self.pairs = None
@@ -25,8 +25,7 @@ class Pipeline:
         self.size = None
 
     # parse source code
-    def get_parsed_source(self, input_file: str,
-                          output_file: str = None) -> pd.DataFrame:
+    def get_parsed_source(self, input_file: str, output_file: str = None) -> pd.DataFrame:
         """Parse code using pycparser
 
         If the user doesn't provide `output_file`, the method reads the
@@ -48,13 +47,15 @@ class Pipeline:
         if output_file is None:
             source = pd.read_pickle(input_path)
         else:
+
             output_path = os.path.join(self.root, self.language, output_file)
-            if self.language == 'c':
+            if self.language == "c":
                 from pycparser import c_parser
+
                 parser = c_parser.CParser()
                 source = pd.read_pickle(input_path)
-                source.columns = ['id', 'code', 'label']
-                source['code'] = source['code'].progress_apply(parser.parse)
+                source.columns = ["id", "code", "label"]
+                source["code"] = source["code"].progress_apply(parser.parse)
                 source.to_pickle(output_path)
             else:
                 import javalang
@@ -64,9 +65,10 @@ class Pipeline:
                     parser = javalang.parser.Parser(tokens)
                     tree = parser.parse_member_declaration()
                     return tree
-                source = pd.read_csv(input_path, delimiter='\t')
-                source.columns = ['id', 'code']
-                source['code'] = source['code'].progress_apply(parse_program)
+
+                source = pd.read_csv(input_path, delimiter="\t")
+                source.columns = ["id", "code"]
+                source["code"] = source["code"].progress_apply(parse_program)
                 source.to_pickle(output_path)
         self.sources = source
         return source
@@ -77,18 +79,17 @@ class Pipeline:
         Args:
             filename (str): [description]
         """
-        pairs = pd.read_pickle(os.path.join(self.root, self.language,
-                                            filename))
+        pairs = pd.read_pickle(os.path.join(self.root, self.language, filename))
         self.pairs = pairs
 
     # split data for training, developing and testing
     def split_data(self):
-        data_path = self.root+self.language+'/'
+        data_path = self.root + "/" + self.language + "/"
         data = self.pairs
         data_num = len(data)
-        ratios = [int(r) for r in self.ratio.split(':')]
-        train_split = int(ratios[0]/sum(ratios)*data_num)
-        val_split = train_split + int(ratios[1]/sum(ratios)*data_num)
+        ratios = [int(r) for r in self.ratio.split(":")]
+        train_split = int(ratios[0] / sum(ratios) * data_num)
+        val_split = train_split + int(ratios[1] / sum(ratios) * data_num)
 
         data = data.sample(frac=1, random_state=666)
         train = data.iloc[:train_split]
@@ -98,35 +99,36 @@ class Pipeline:
         def check_or_create(path):
             if not os.path.exists(path):
                 os.mkdir(path)
-        train_path = data_path+'train/'
+
+        train_path = data_path + "train/"
         check_or_create(train_path)
-        self.train_file_path = train_path+'train_.pkl'
+        self.train_file_path = train_path + "train_.pkl"
         train.to_pickle(self.train_file_path)
 
-        dev_path = data_path+'dev/'
+        dev_path = data_path + "dev/"
         check_or_create(dev_path)
-        self.dev_file_path = dev_path+'dev_.pkl'
+        self.dev_file_path = dev_path + "dev_.pkl"
         dev.to_pickle(self.dev_file_path)
 
-        test_path = data_path+'test/'
+        test_path = data_path + "test/"
         check_or_create(test_path)
-        self.test_file_path = test_path+'test_.pkl'
+        self.test_file_path = test_path + "test_.pkl"
         test.to_pickle(self.test_file_path)
 
     # construct dictionary and train word embedding
     def dictionary_and_embedding(self, input_file, size):
         self.size = size
-        data_path = self.root+self.language+'/'
+        data_path = self.root + "/" + self.language + "/"
         if not input_file:
             input_file = self.train_file_path
         pairs = pd.read_pickle(input_file)
-        train_ids = pairs['id1'].append(pairs['id2']).unique()
+        train_ids = pairs["id1"].append(pairs["id2"]).unique()
 
-        trees = self.sources.set_index('id', drop=False).loc[train_ids]
-        if not os.path.exists(data_path+'train/embedding'):
-            os.mkdir(data_path+'train/embedding')
-        if self.language == 'c':
-            sys.path.append('../')
+        trees = self.sources.set_index("id", drop=False).loc[train_ids]
+        if not os.path.exists(data_path + "train/embedding"):
+            os.mkdir(data_path + "train/embedding")
+        if self.language == "c":
+            sys.path.append("../")
             from prepare_data import get_sequences as func
         else:
             from utils import get_sequence as func
@@ -135,27 +137,31 @@ class Pipeline:
             sequence = []
             func(ast, sequence)
             return sequence
-        corpus = trees['code'].apply(trans_to_sequences)
-        str_corpus = [' '.join(c) for c in corpus]
-        trees['code'] = pd.Series(str_corpus)
+
+        corpus = trees["code"].apply(trans_to_sequences)
+        str_corpus = [" ".join(c) for c in corpus]
+        trees["code"] = pd.Series(str_corpus)
         # trees.to_csv(data_path+'train/programs_ns.tsv')
 
         from gensim.models.word2vec import Word2Vec
-        w2v = Word2Vec(corpus, size=size, workers=16, sg=1,
-                       max_final_vocab=3000)
-        w2v.save(data_path+'train/embedding/node_w2v_' + str(size))
+
+        w2v = Word2Vec(corpus, size=size, workers=16, sg=1, max_final_vocab=3000)
+        w2v.save(data_path + "train/embedding/node_w2v_" + str(size))
 
     # generate block sequences with index representations
     def generate_block_seqs(self):
-        if self.language == 'c':
+        if self.language == "c":
             from prepare_data import get_blocks as func
         else:
             from utils import get_blocks_v1 as func
         from gensim.models.word2vec import Word2Vec
 
         word2vec = Word2Vec.load(
-            self.root + self.language+'/train/embedding/node_w2v_' +
-            str(self.size)
+            self.root
+            + "/"
+            + self.language
+            + "/train/embedding/node_w2v_"
+            + str(self.size)
         ).wv
         vocab = word2vec.vocab
         max_token = word2vec.syn0.shape[0]
@@ -176,62 +182,59 @@ class Pipeline:
                 btree = tree_to_index(b)
                 tree.append(btree)
             return tree
+
         trees = pd.DataFrame(self.sources, copy=True)
-        trees['code'] = trees['code'].apply(trans2seq)
-        if 'label' in trees.columns:
-            trees.drop('label', axis=1, inplace=True)
+        trees["code"] = trees["code"].apply(trans2seq)
+        if "label" in trees.columns:
+            trees.drop("label", axis=1, inplace=True)
         self.blocks = trees
 
     # merge pairs
     def merge(self, data_path, part):
         pairs = pd.read_pickle(data_path)
-        pairs['id1'] = pairs['id1'].astype(int)
-        pairs['id2'] = pairs['id2'].astype(int)
-        df = pd.merge(pairs, self.blocks, how='left',
-                      left_on='id1', right_on='id')
-        df = pd.merge(df, self.blocks, how='left',
-                      left_on='id2', right_on='id')
-        df.drop(['id_x', 'id_y'], axis=1, inplace=True)
+        pairs["id1"] = pairs["id1"].astype(int)
+        pairs["id2"] = pairs["id2"].astype(int)
+        df = pd.merge(pairs, self.blocks, how="left", left_on="id1", right_on="id")
+        df = pd.merge(df, self.blocks, how="left", left_on="id2", right_on="id")
+        df.drop(["id_x", "id_y"], axis=1, inplace=True)
         df.dropna(inplace=True)
 
-        df.to_pickle(self.root+self.language+'/'+part+'/blocks.pkl')
+        df.to_pickle(self.root + "/" + self.language + "/" + part + "/blocks.pkl")
 
     # run for processing data to train
     def run(self):
-        print('parse source code...')
-        input_file = (
-            'programs.pkl' if self.language == 'c' else 'bcb_funcs_all.tsv'
-        )
-        if os.path.exists(os.path.join(self.root, self.language, 'ast.pkl')):
-            print('a')
-            self.get_parsed_source(input_file='ast.pkl')
+        print("parse source code...")
+        input_file = "programs.pkl" if self.language == "c" else "bcb_funcs_all.tsv"
+        if os.path.exists(os.path.join(self.root, self.language, "ast.pkl")):
+            self.get_parsed_source(input_file="ast.pkl")
         else:
-            self.get_parsed_source(input_file=input_file,
-                                   output_file='ast.pkl')
-        print('read id pairs...')
-        if self.language == 'c':
-            self.read_pairs('oj_clone_ids.pkl')
+            self.get_parsed_source(input_file=input_file, output_file="ast.pkl")
+        print("read id pairs...")
+        if self.language == "c":
+            self.read_pairs("oj_clone_ids.pkl")
         else:
-            self.read_pairs('bcb_pair_ids.pkl')
-        print('split data...')
+            self.read_pairs("bcb_pair_ids.pkl")
+        print("split data...")
         self.split_data()
-        print('train word embedding...')
+        print("train word embedding...")
         self.dictionary_and_embedding(None, 128)
-        print('generate block sequences...')
+        print("generate block sequences...")
         self.generate_block_seqs()
-        print('merge pairs and blocks...')
-        self.merge(self.train_file_path, 'train')
-        self.merge(self.dev_file_path, 'dev')
-        self.merge(self.test_file_path, 'test')
+        print("merge pairs and blocks...")
+        self.merge(self.train_file_path, "train")
+        self.merge(self.dev_file_path, "dev")
+        self.merge(self.test_file_path, "test")
 
 
-@click.command()
-@click.option('--lang', required=True, type=str,
-              help="Language for the code input ('c' or 'java')")
+# @click.command()
+# @click.option(
+#     "--lang", required=True, type=str, help="Language for the code input ('c' or 'java')"
+# )
 def main(lang):
-    ppl = Pipeline('3:1:1', 'data', str(lang))
+    ppl = Pipeline("3:1:1", "data", str(lang))
     ppl.run()
 
 
 if __name__ == "__main__":
-    main()
+    main("c")
+
